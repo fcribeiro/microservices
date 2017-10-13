4537c71347652bd1de7cb0b7b525c6495aa9ae28
Fabio Ribeiro <fdcr@student.dei.uc.pt>
2017-10-13 17:06:33 +0100

    Playlists_MS pre-weekend commit

--- Send_Requests/business/Business.py
+++ Send_Requests/business/Business.py
@@ -3,8 +3,7 @@ import logging
 import connexion
 import requests
 import json
-# from flask_jwt import JWT, jwt_required, current_identity
-# from werkzeug.security import safe_str_cmp
+
 
 # Logging configuration
 logging.basicConfig(datefmt='%d/%m/%Y %I:%M:%S', level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
@@ -41,7 +40,6 @@ app = connexion.App(__name__)
 app.add_api('swagger.yaml')
 application = app.app
 
-# jwt = JWT(application, authenticate, identity)
 
 # config
 application.config.update(
