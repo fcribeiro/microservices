8603e90054123ee185e4432ab17315273098cbaa
jaimelive <jaime@live.com.pt>
2017-11-29 10:37:27 +0000

    Push to repo

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#Build Images
 ( cd Users_MS && docker build -t jaimelive/users_ms . )
 ( cd Main_App && docker build -t jaimelive/main_app . )
-
+#Push images
+docker push jaimelive/users_ms
+docker push jaimelive/main_app
